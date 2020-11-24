d0622ba1b10b143f180def6d404807a72cacb573
nick repasy <nrepasy@gmail.com>
2020-11-24 19:11:09 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,24 +1,29 @@
 def addition(num1, num2)
   sum = num1 + num2
   puts sum
-end
+  end
 
 def subtraction(num1, num2)
-
-end
+  sum = num1 - num2
+  puts sum
+  end
 
 def division(num1, num2)
-
-end
+  sum = num1 * num2
+  puts sum
+  end
 
 def multiplication(num1, num2)
-
-end
+  sum = num1* num2
+  puts sum
+  end
 
 def modulo(num1, num2)
-
-end
+  sum = num1 % num2
+  puts sum
+  end
 
 def square_root(num)
+  
 
 end
