a1fd9fcb9b06a15ae44c5616d97934142a8dad7b
nick repasy <nrepasy@gmail.com>
2020-11-24 19:17:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -14,14 +14,14 @@ def subtraction(num1, num2)
 
 def division(num1, num2)
   sum = num1 * num2
-  puts sum
+  p sum
   end
   
   division(50,2)
 
 def multiplication(num1, num2)
   sum = num1* num2
-  puts sum
+  p sum
   end
 
 multiplication(4,30)
@@ -34,7 +34,7 @@ def modulo(num1, num2)
   modulo(34,5)
 
 def square_root(num)
-  puts Math.sqrt(num)
+  p Math.sqrt(num)
 end
   
   square_root(81)
